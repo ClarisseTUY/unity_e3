2cf8807ba880c31b900dfd4b5a5c3658f04d8a73
ClarisseTUY <clarisse.tuy@edu.esiee.fr>
2024-10-25 19:23:52 +0200

    night cycle
    entre jour et nuit : compètement noir

--- Projet/Assets/Scripts/DayAndNightCycle.cs
+++ Projet/Assets/Scripts/DayAndNightCycle.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.Rendering.HighDefinition;
 
 public class DayAndNightCycle : MonoBehaviour
@@ -12,28 +13,41 @@ public class DayAndNightCycle : MonoBehaviour
     [Header("CurrentTine")]
     public string currentTimeString;
 
-    [Header("Light Settings")]
+    [Header("Sun Settings")]
     public Light sunLight;
     public float sunPosition = 1f;
     public float sunIntensity = 1f;
+
     public AnimationCurve sunIntensityMultiplier;
-    public AnimationCurve lightTemperatureCurse;
+    public AnimationCurve sunTemperatureCurve;
+
 
     public bool isDay = true;
-    
+    public bool sunActive = true;
+    public bool moonActive = true;
+
+
+    [Header("Moon Settings")]
+    public Light moonLight;
+    public float moonIntensity = 1f;
+
+    public AnimationCurve moonIntensityMultiplier;
+    public AnimationCurve moonTemperatureCurve;
+
+
     // Start is called before the first frame update
     void Start()
     {
         UpdateTimeText();
         CheckShadowStatus();
     }
-    
+
     // Update is called once per frame
     void Update()
     {
         currentTime += Time.deltaTime * timeSpeed;
-            if(currentTime >=24)
-                currentTime = 0;
+        if (currentTime >= 24)
+            currentTime = 0;
         UpdateTimeText();
         UpdateLight();
         CheckShadowStatus();
@@ -44,30 +58,49 @@ public class DayAndNightCycle : MonoBehaviour
         CheckShadowStatus();
     }
 
-    void UpdateTimeText() {
-        currentTimeString = Mathf.Floor(currentTime).ToString("00") + ":"+((currentTime % 1) * 60). ToString("00");
+    void UpdateTimeText()
+    {
+        currentTimeString = Mathf.Floor(currentTime).ToString("00") + ":" + ((currentTime % 1) * 60).ToString("00");
     }
 
     void UpdateLight()
     {
         float sunRotation = currentTime / 24f * 360f;
         sunLight.transform.rotation = Quaternion.Euler(sunRotation - 90f, sunPosition, 0f);
+        moonLight.transform.rotation = Quaternion.Euler(sunRotation + 90f, sunPosition, 0f);
 
         float normalizedTime = currentTime / 24f;
-        float IntensityCurve = sunIntensityMultiplier.Evaluate(normalizedTime);
-        HDAdditionalLightData sunLightData = sunLight.GetComponent<HDAdditionalLightData>();
+        float sunIntensityCurve = sunIntensityMultiplier.Evaluate(normalizedTime);
+        float moonIntensityCurve = moonIntensityMultiplier.Evaluate(normalizedTime);
 
-        if(sunLightData!=null)
+        HDAdditionalLightData sunLightData = sunLight.GetComponent<HDAdditionalLightData>();
+        HDAdditionalLightData moonLightData = moonLight.GetComponent<HDAdditionalLightData>();
+
+        if (sunLightData != null)
         {
-            sunLightData.intensity = IntensityCurve*sunIntensity;
+            sunLightData.intensity = sunIntensityCurve * sunIntensity;
         }
 
-        float temperatureMultiplier = lightTemperatureCurse.Evaluate(normalizedTime);
-        Light lightComponent = sunLight.GetComponent<Light>();
-
-        if(lightComponent!=null)
+        if (moonLightData != null)
         {
-            lightComponent.colorTemperature = temperatureMultiplier * 10000f;
+            moonLightData.intensity = moonIntensityCurve * moonIntensity;
+        }
+
+        float sunTemperatureMultiplier = sunTemperatureCurve.Evaluate(normalizedTime);
+        float moonTemperatureMultiplier = moonTemperatureCurve.Evaluate(normalizedTime);
+
+        Light sunLightComponent = sunLight.GetComponent<Light>();
+        Light moonLightComponent = moonLight.GetComponent<Light>();
+
+        if (sunLightComponent != null)
+        {
+            sunLightComponent.colorTemperature = sunTemperatureMultiplier * 10000f;
+        }
+
+
+        if (moonLightComponent != null)
+        {
+            moonLightComponent.colorTemperature = moonTemperatureMultiplier * 10000f;
         }
 
     }
@@ -75,17 +108,45 @@ public class DayAndNightCycle : MonoBehaviour
     void CheckShadowStatus()
     {
         HDAdditionalLightData sunLightData = sunLight.GetComponent<HDAdditionalLightData>();
+        HDAdditionalLightData moonLightData = moonLight.GetComponent<HDAdditionalLightData>();
+
         float currentSunRotation = currentTime;
-        if(currentSunRotation >= 6f && currentSunRotation <= 10f)
+        if (currentSunRotation >= 6f && currentSunRotation <= 18f)
         {
             sunLightData.EnableShadows(true);
+            moonLightData.EnableShadows(false);
             isDay = true;
         }
         else
         {
             sunLightData.EnableShadows(false);
+            moonLightData.EnableShadows(true);
             isDay = false;
         }
+
+        if (currentSunRotation >= 5.7f && currentSunRotation <= 18.3f)
+        {
+            sunLight.gameObject.SetActive(true);
+            sunActive = true;
+        }
+        else
+        {
+            sunLight.gameObject.SetActive(false);
+            sunActive = false;
+        }
+
+        if (currentSunRotation >= 6.3f && currentSunRotation <= 17.7f)
+        {
+            moonLight.gameObject.SetActive(false);
+            moonActive = false;
+        }
+        else
+        {
+            moonLight.gameObject.SetActive(true);
+            moonActive = true;
+        }
+
+
     }
 
 
