30e49e866604f000544098da4b7b985239c8ebe5
Clarisse <clarisse.tuy@edu.esiee.fr>
2025-05-16 18:52:22 +0200

    Ajout du lapin + animations

---
+++ Projet/Assets/Scripts/RabbitInHand.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class RabbitInHand : MonoBehaviour
+{
+    [SerializeField] Animator rabbitInHand;
+    // Start is called before the first frame update
+    void Start()
+    {
+        XRGrabInteractable grabbable = GetComponent<XRGrabInteractable>();
+        grabbable.activated.AddListener(RabbitDeath);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void RabbitDeath(ActivateEventArgs arg)
+    {
+        rabbitInHand.SetTrigger("death");
+    }
+}
