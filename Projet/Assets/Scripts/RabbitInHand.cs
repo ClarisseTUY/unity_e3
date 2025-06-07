fc34bdf2da5ee324b264a4d54c7f1e0ef53dfdd5
ClarisseTUY <clarisse.tuy@edu.esiee.fr>
2025-06-07 16:05:43 +0200

    update attach point rabbit death

--- Projet/Assets/Scripts/RabbitInHand.cs
+++ Projet/Assets/Scripts/RabbitInHand.cs
@@ -6,21 +6,46 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class RabbitInHand : MonoBehaviour
 {
     [SerializeField] Animator rabbitInHand;
-    // Start is called before the first frame update
+    [SerializeField] Transform attachPoint;
+    [SerializeField] Transform deathAttachPoint;
+
+    private XRGrabInteractable grabbable;
+    private bool isDead = false;
+
     void Start()
     {
-        XRGrabInteractable grabbable = GetComponent<XRGrabInteractable>();
+        grabbable = GetComponent<XRGrabInteractable>();
+        grabbable.attachTransform = attachPoint;
         grabbable.activated.AddListener(RabbitDeath);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 
     public void RabbitDeath(ActivateEventArgs arg)
     {
+        if (isDead) return;
+        isDead = true;
+
         rabbitInHand.SetTrigger("Death");
+
+        // Si une main tient le lapin
+        if (grabbable.isSelected)
+        {
+            // Obtenir l'interactor qui tient l'objet (main ou socket)
+            IXRSelectInteractor interactor = grabbable.firstInteractorSelecting;
+            XRInteractionManager interactionManager = grabbable.interactionManager;
+
+            // 1. Forcer le release
+            interactionManager.SelectExit(interactor, grabbable);
+
+            // 2. Changer le point d'attache
+            grabbable.attachTransform = deathAttachPoint;
+
+            // 3. Forcer la main à reprendre
+            interactionManager.SelectEnter(interactor, grabbable);
+        }
+        else
+        {
+            // Si pas tenu, juste changer l’attach point
+            grabbable.attachTransform = deathAttachPoint;
+        }
     }
 }
